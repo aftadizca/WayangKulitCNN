a6e17b5f6e363d373c2d67fba68d2dbc9ed899f8
aftadizca <aftadizca@gmail.com>
2020-04-10 18:06:55 +0700

    redesign

--- src/components/ButtonMenu.js
+++ src/components/ButtonMenu.js
@@ -1,36 +1,23 @@
 import React from 'react';
-import { TouchableNativeFeedback, View, Text } from 'react-native';
+import { TouchableNativeFeedback, View, Text, StyleSheet } from 'react-native';
+import { Icon } from 'native-base';
 import PropTypes from 'prop-types';
-import { colors, styles } from 'config';
+import { COLORS, FONTS, ICONS } from '../config';
 
 function ButtonMenu(props) {
 	return (
 		<TouchableNativeFeedback
-			background={TouchableNativeFeedback.Ripple(colors.SECONDARY_DARK, false)}
+			background={TouchableNativeFeedback.Ripple(COLORS.PRIMARY, false)}
 			useForeground={true}
 			onPress={props.onPress}>
-			<View
-				style={{
-					height: 150,
-					padding: 15,
-					width: 128,
-					margin: 20,
-					flexDirection: 'column',
-					alignItems: 'center',
-					justifyContent: 'space-evenly',
-					borderRadius: 8,
-					overflow: 'hidden',
-				}}>
-				{props.children}
-				<Text
-					style={{
-						...styles.buttonPrimaryText,
-						color: colors.PRIMARY_DARK,
-						textAlign: 'center',
-						fontSize: 18,
-					}}>
-					{props.text}
-				</Text>
+			<View style={styles.viewMenu}>
+				<View style={styles.iconView}>{props.children}</View>
+				<Text style={styles.textPrimary}>{props.text}</Text>
+				<Icon
+					name={ICONS.ARROW_RIGHT.name}
+					type={ICONS.ARROW_RIGHT.type}
+					style={styles.icon}
+				/>
 			</View>
 		</TouchableNativeFeedback>
 	);
@@ -40,5 +27,38 @@ ButtonMenu.propTypes = {
 	text: PropTypes.string.isRequired,
 	onPress: PropTypes.func,
 };
+
+const styles = StyleSheet.create({
+	viewMenu: {
+		height: 100,
+		padding: 15,
+		margin: 10,
+		width: '70%',
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-around',
+		borderRadius: 20,
+		borderWidth: 2,
+		borderColor: COLORS.SECONDARY,
+		overflow: 'hidden',
+	},
+	iconView: {
+		width: 72,
+		height: 72,
+		flex: 1,
+	},
+	textPrimary: {
+		fontFamily: FONTS.MEDIUM,
+		color: COLORS.SECONDARY,
+		textAlign: 'left',
+		marginLeft: 15,
+		fontSize: 18,
+		flex: 2,
+	},
+	icon: {
+		color: COLORS.SECONDARY,
+		fontSize: 24,
+	},
+});
 
 export default ButtonMenu;

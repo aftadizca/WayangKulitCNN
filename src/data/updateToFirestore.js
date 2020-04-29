391ae8d88d578974b82b809e739bd94369bcca70
aftadizca <aftadizca@gmail.com>
2020-04-29 20:37:21 +0700

    testing ke firebase

---
+++ src/data/updateToFirestore.js
@@ -0,0 +1,25 @@
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
+const data = require('./dataWayang.json');
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: "AIzaSyCFRfm1ZTG4fsBY1mBN6FGUGa9COaAC_3k", //CHANGE THIS
+    authDomain: "wayangkulitcnn.firebaseapp.com", //CHANGE THIS
+    projectId: "wayangkulitcnn" //CHANGE THIS
+});
+
+var db = firebase.firestore();
+
+console.log(data);
+
+data.forEach(function (obj, i) {
+    console.log(i);
+    db.collection("menu").doc(i.toString()).set(obj).then(function (docRef) {
+        console.log("Document written");
+    }).catch(function (error) {
+        console.error("Error adding document: ", error);
+    });
+});

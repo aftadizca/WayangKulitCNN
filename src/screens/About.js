3e25a60ea2a94ba759f8baef4cefc96f457be7b1
aftadizca <aftadizca@gmail.com>
2020-05-09 04:07:11 +0700

    redesign

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Image, StyleSheet, StatusBar } from 'react-native';
 import Text, { useScaleText } from 'react-native-text';
 import { Grid, Row, Col } from 'react-native-easy-grid';
 import { COLORS, FONTS, ICONS } from '../config';
 import { version } from '../../package.json';
-import { MyButton } from '../components';
 import {
 	heightPercentageToDP,
 	widthPercentageToDP,
 } from 'react-native-responsive-screen';
 import {
-	Container,
-	Header,
-	Content,
 	Button,
 	ListItem,
+	List,
 	Icon,
 	Left,
 	Body,
@@ -24,93 +21,93 @@ import {
 import Avatar from '../icon/avatar.jpg';
 import BG from '../icon/bg2.svg';
 
-export class About extends Component {
-	render() {
-		return (
-			<Grid>
-				<StatusBar backgroundColor={COLORS.PRIMARY_DARK} />
-				<Row size={3} style={styles.rOne}>
-					<Col
-						style={{
-							padding: 5,
-							justifyContent: 'center',
-							alignItems: 'center',
-						}}>
-						<Image
-							source={Avatar}
-							resizeMethod='resize'
-							resizeMode='stretch'
-							style={styles.image}
-						/>
-					</Col>
-					<Col>
-						<Text adjustsFontSizeToFit style={styles.nameText}>
-							Afta Dizca Wahana
+function About() {
+	return (
+		<Grid>
+			<StatusBar backgroundColor={COLORS.PRIMARY_DARK} />
+			<Row size={3} style={styles.rOne}>
+				<Col
+					style={{
+						padding: 5,
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}>
+					<Image
+						source={Avatar}
+						resizeMethod='resize'
+						resizeMode='stretch'
+						style={styles.image}
+					/>
+				</Col>
+				<Col>
+					<Text adjustsFontSizeToFit style={styles.nameText}>
+						Afta Dizca Wahana
 						</Text>
-						<Text style={styles.nameSubText}>Teknik Informatika</Text>
-						<Text style={styles.nameSubText}>UMAHA</Text>
-					</Col>
-				</Row>
-				<Row size={2}>
-					<View style={styles.viewBackground}>
-						<BG />
-					</View>
-				</Row>
-				<Row size={4} style={styles.rThree}>
+					<Text style={styles.nameSubText}>Teknik Informatika</Text>
+					<Text style={styles.nameSubText}>UMAHA</Text>
+				</Col>
+			</Row>
+			<Row size={2}>
+				<View style={styles.viewBackground}>
+					<BG />
+				</View>
+			</Row>
+			<Row size={4} style={styles.rThree}>
+				<List>
 					<AboutList
 						title='Versi Aplikasi'
 						desc={version}
 						iconLeft={ICONS.APP_VERSION}
 					/>
 					<AboutList
-						title='Versi Model CNN'
+						title='Versi Model'
 						desc={'1.0'}
 						iconRight={ICONS.UPDATE}
 						iconLeft={ICONS.CNN_VERSION}
 					/>
-				</Row>
-			</Grid>
-		);
-	}
+				</List>
+
+			</Row>
+		</Grid>
+	);
 }
+
 
 function AboutList(props) {
 	const { iconLeft, iconRight, title, desc } = props;
 	return (
 		<ListItem
 			icon
+			noIndent
 			style={{
-				width: '70%',
+				width: '100%',
 				marginBottom: 10,
+				height: heightPercentageToDP('10%'),
+				backgroundColor: COLORS.PRIMARY_DARK,
+				borderRadius: widthPercentageToDP('50%')
 			}}>
 			<Left
 				style={{
-					width: '17%',
-					justifyContent: 'center',
-					alignItems: 'center',
-					paddingRight: 0,
+					width: '25%',
 				}}>
 				<Icon
 					style={{
-						fontSize: useScaleText({ fontSize: 30 }).fontSize,
-						color: COLORS.PRIMARY_DARK,
-						height: '100%',
-						width: '100%',
+						color: COLORS.PRIMARY_LIGHT
 					}}
 					{...iconLeft}
 				/>
 			</Left>
 			<Body style={{ borderBottomWidth: 0 }}>
-				<Text style={styles.listPrimaryText}>{title}</Text>
-				<Text style={styles.listSecondaryText}>{desc}</Text>
+				<Text style={{ fontFamily: FONTS.REGULAR, fontSize: 18, color: COLORS.PRIMARY_LIGHT }}>{title}</Text>
+				<Text style={{ fontFamily: FONTS.CONDENSED, fontSize: 16, color: COLORS.PRIMARY_LIGHT }}>{desc}</Text>
 			</Body>
 			<Right style={{ borderBottomWidth: 0 }}>
 				{iconRight && (
 					<Button
 						rounded
-						small
-						style={{ backgroundColor: COLORS.PRIMARY_DARK }}>
-						<Icon active {...iconRight} />
+						transparent
+					>
+						<Icon style={{ color: COLORS.PRIMARY_LIGHT }} {...iconRight} />
 					</Button>
 				)}
 			</Right>

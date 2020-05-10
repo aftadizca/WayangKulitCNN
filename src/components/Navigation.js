5290f227eeb6503b18f5d548ac4d863ddea498f2
aftadizca <aftadizca@gmail.com>
2020-05-10 22:13:11 +0700

    enlarge icon

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,16 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import { Button, Icon } from 'native-base';
 import { COLORS, ICONS } from '../config';
+import { widthPercentageToDP } from 'react-native-responsive-screen';
 
 export default function Navigation(props) {
 	const { navigation } = props;
-	const [opacity, setOpacity] = useState(0.3);
+	const [opacity, setOpacity] = useState(0.1);
 
 	function handleOpacity(bool) {
 		if (bool) {
 			setOpacity(1);
 		} else {
-			setOpacity(0.3);
+			setOpacity(0.1);
 		}
 	}
 
@@ -19,8 +20,8 @@ export default function Navigation(props) {
 		<View
 			style={{
 				position: 'absolute',
-				top: 0,
-				left: 0,
+				top: widthPercentageToDP('2%'),
+				left: widthPercentageToDP('2%'),
 				zIndex: 100,
 				flexDirection: 'row',
 			}}>
@@ -33,7 +34,12 @@ export default function Navigation(props) {
 					onPressOut={() => handleOpacity(false)}
 					style={{ opacity: opacity }}>
 					<Icon
-						style={{ color: COLORS.PRIMARY_LIGHT }}
+						style={{
+							color: COLORS.PRIMARY_LIGHT,
+							fontSize: 32,
+							marginLeft: 6,
+							marginRight: 6,
+						}}
 						{...ICONS.NAVIGATION_BACK}
 					/>
 				</Button>
@@ -47,7 +53,7 @@ export default function Navigation(props) {
 					onPressOut={() => handleOpacity(false)}
 					style={{ opacity: opacity, marginLeft: props.back ? -15 : 0 }}>
 					<Icon
-						style={{ color: COLORS.PRIMARY_LIGHT }}
+						style={{ color: COLORS.PRIMARY_LIGHT, fontSize: 100 }}
 						{...ICONS.NAVIGATION_HOME}
 					/>
 				</Button>

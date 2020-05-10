55f782c63d8a40a933cfa3bcd347a932a9f342c8
aftadizca <aftadizca@gmail.com>
2020-05-10 22:12:37 +0700

    fix list

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,6 +1,12 @@
 /* eslint-disable no-undef */
 import React, { useState, useEffect } from 'react';
-import { View, Image, StyleSheet, StatusBar } from 'react-native';
+import {
+	View,
+	Image,
+	StyleSheet,
+	StatusBar,
+	TouchableNativeFeedback,
+} from 'react-native';
 import Text from 'react-native-text';
 import { Grid, Row, Col } from 'react-native-easy-grid';
 import { COLORS, FONTS, ICONS } from '../config';
@@ -11,7 +17,7 @@ import {
 	heightPercentageToDP,
 	widthPercentageToDP,
 } from 'react-native-responsive-screen';
-import { Button, ListItem, List, Icon, Left, Body, Right } from 'native-base';
+import { Button, Icon } from 'native-base';
 import Avatar from '../icon/avatar.jpg';
 import BG from '../icon/bg2.svg';
 
@@ -73,19 +79,17 @@ function About(props) {
 				</View>
 			</Row>
 			<Row size={4} style={styles.rThree}>
-				<List>
-					<AboutList
-						title="Versi Aplikasi"
-						desc={version}
-						iconLeft={ICONS.APP_VERSION}
-					/>
-					<AboutList
-						title="Versi Model"
-						desc={modelName}
-						iconRight={ICONS.UPDATE}
-						iconLeft={ICONS.CNN_VERSION}
-					/>
-				</List>
+				<AboutList
+					title="Versi Aplikasi"
+					desc={version}
+					iconLeft={ICONS.APP_VERSION}
+				/>
+				<AboutList
+					title="Versi Model"
+					desc={modelName}
+					iconRight={ICONS.UPDATE}
+					iconLeft={ICONS.CNN_VERSION}
+				/>
 			</Row>
 		</Grid>
 	);
@@ -94,56 +98,74 @@ function About(props) {
 function AboutList(props) {
 	const { iconLeft, iconRight, title, desc } = props;
 	return (
-		<ListItem
-			icon
-			noIndent
-			style={{
-				width: '100%',
-				marginBottom: 10,
-				height: heightPercentageToDP('10%'),
-				backgroundColor: COLORS.PRIMARY_DARK,
-				borderRadius: widthPercentageToDP('50%'),
-			}}>
-			<Left
-				style={{
-					width: '25%',
-				}}>
-				<Icon
-					style={{
-						color: COLORS.PRIMARY_LIGHT,
-					}}
-					{...iconLeft}
-				/>
-			</Left>
-			<Body style={{ borderBottomWidth: 0 }}>
-				<Text
-					style={{
-						fontFamily: FONTS.REGULAR,
-						fontSize: 18,
-						color: COLORS.PRIMARY_LIGHT,
-					}}>
-					{title}
-				</Text>
-				<Text
-					style={{
-						fontFamily: FONTS.CONDENSED,
-						fontSize: 16,
-						opacity: 0.7,
-						color: COLORS.PRIMARY_LIGHT,
-					}}>
-					{desc}
-				</Text>
-			</Body>
-			<Right style={{ borderBottomWidth: 0 }}>
+		<View style={listStyles.container}>
+			<View style={listStyles.left}>
+				<Icon style={listStyles.iconLeft} {...iconLeft} />
+			</View>
+			<View style={listStyles.center}>
+				<Text style={listStyles.centerTextTop}>{title}</Text>
+				<Text style={listStyles.centerTextBottom}>{desc}</Text>
+			</View>
+			<View style={listStyles.right}>
 				{iconRight && (
-					<Button rounded transparent>
-						<Icon style={{ color: COLORS.PRIMARY_LIGHT }} {...iconRight} />
-					</Button>
+					<TouchableNativeFeedback
+						background={TouchableNativeFeedback.Ripple(
+							COLORS.PRIMARY_LIGHT,
+							false
+						)}
+						useForeground={true}>
+						<View style={listStyles.rightIconView}>
+							<Icon style={listStyles.rightIcon} {...iconRight} />
+						</View>
+					</TouchableNativeFeedback>
 				)}
-			</Right>
-		</ListItem>
+			</View>
+		</View>
 	);
 }
+
+const listStyles = StyleSheet.create({
+	container: {
+		width: '80%',
+		marginBottom: 10,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'center',
+		height: heightPercentageToDP('10%'),
+		backgroundColor: COLORS.PRIMARY_DARK,
+		borderRadius: widthPercentageToDP('50%'),
+	},
+	iconLeft: {
+		color: COLORS.PRIMARY_LIGHT,
+	},
+	left: {
+		width: '25%',
+		alignItems: 'center',
+	},
+	center: { width: '50%' },
+	centerTextTop: {
+		fontFamily: FONTS.REGULAR,
+		fontSize: 18,
+		color: COLORS.PRIMARY_LIGHT,
+	},
+	centerTextBottom: {
+		fontFamily: FONTS.LIGHT,
+		fontSize: 16,
+		opacity: 0.7,
+		color: COLORS.PRIMARY_LIGHT,
+	},
+	right: {
+		width: '25%',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	rightIconView: { borderRadius: 100, padding: 5, overflow: 'hidden' },
+	rightIcon: {
+		color: COLORS.PRIMARY_LIGHT,
+		marginLeft: 0,
+		marginRight: 0,
+	},
+});
 
 const styles = StyleSheet.create({
 	rOne: {
@@ -165,7 +187,7 @@ const styles = StyleSheet.create({
 		color: COLORS.PRIMARY_LIGHT,
 	},
 	nameSubText: {
-		fontFamily: FONTS.CONDENSED,
+		fontFamily: FONTS.LIGHT,
 		fontSize: 16,
 		color: COLORS.PRIMARY_LIGHT,
 		opacity: 0.5,

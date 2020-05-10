1af15b0573c87bfdc298b9eee41c819872edecd1
aftadizca <aftadizca@gmail.com>
2020-05-10 22:12:46 +0700

    fix

--- src/components/ButtonMenu.js
+++ src/components/ButtonMenu.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { TouchableNativeFeedback, View, StyleSheet } from 'react-native';
 import { Icon } from 'native-base';
-import Text from 'react-native-text'
+import Text from 'react-native-text';
 import PropTypes from 'prop-types';
-import LinearGradient from 'react-native-linear-gradient'
+import LinearGradient from 'react-native-linear-gradient';
 import { COLORS, FONTS, ICONS } from '../config';
 import {
 	heightPercentageToDP,
@@ -16,7 +16,11 @@ function ButtonMenu(props) {
 			background={TouchableNativeFeedback.Ripple(COLORS.PRIMARY, false)}
 			useForeground={true}
 			onPress={props.onPress}>
-			<LinearGradient colors={COLORS.GRADIENT} useAngle={true} angle={180} style={styles.viewMenu}>
+			<LinearGradient
+				colors={COLORS.GRADIENT}
+				useAngle={true}
+				angle={180}
+				style={styles.viewMenu}>
 				<View style={styles.iconView}>{props.icon}</View>
 				<View style={styles.textView}>
 					<Text style={styles.textPrimary}>{props.text}</Text>
@@ -36,7 +40,7 @@ ButtonMenu.propTypes = {
 	text: PropTypes.string.isRequired,
 	caption: PropTypes.string.isRequired,
 	onPress: PropTypes.func,
-	icon: PropTypes.element.isRequired
+	icon: PropTypes.element.isRequired,
 };
 
 const styles = StyleSheet.create({
@@ -51,33 +55,33 @@ const styles = StyleSheet.create({
 		overflow: 'hidden',
 		elevation: 10,
 		borderWidth: 1,
-		backgroundColor: COLORS.PRIMARY_DARK
+		backgroundColor: COLORS.PRIMARY_DARK,
 	},
 	iconView: {
 		width: '40%',
 		height: '40%',
 		flex: 30,
 		justifyContent: 'center',
-		alignItems: 'center'
+		alignItems: 'center',
 	},
 
 	textView: { flex: 55 },
 	icon: {
 		flex: 15,
 		color: COLORS.PRIMARY_DARK,
-		fontSize: 24
+		fontSize: 24,
 	},
 	textPrimary: {
 		fontFamily: FONTS.BOLD,
 		color: COLORS.PRIMARY_DARK,
 		textAlign: 'left',
-		fontSize: 16
+		fontSize: 16,
 	},
 	textSecondary: {
 		fontFamily: FONTS.CONDENSED,
 		color: COLORS.PRIMARY_DARK,
 		textAlign: 'left',
-		fontSize: 14
+		fontSize: 14,
 	},
 });
 
